627bb573700228e9712c7fd0725982393f0fb37c
Dewmini Thakshila Kavindi <99270120+KavindiDev@users.noreply.github.com>
2024-07-10 21:27:50 +0530

    about section added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Home/Hero"
+import About from "@/components/Home/About"
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   )
 }
--- components/Home/About.jsx
+++ components/Home/About.jsx
@@ -1,13 +1,11 @@
 export default function About() {
     return (
-        <section id="about" className="py-16 md:py-24 mx-auto items-center justify-center">
+        <section id="about" className="pt-16 md:pt-24 mx-auto items-center justify-center">
             <div className="container mx-auto flex flex-col items-center justify-center px-4 md:flex-row md:px-6">
                 <div className="md:w-1/2">
                     <h2 className="text-3xl font-bold tracking-tight md:text-4xl">About Me</h2>
                     <p className="mt-4 md:text-lg">
-                        I am a passionate software engineer with a strong background in full-stack development. I specialize in
-                        building high-performance, scalable, and user-friendly web applications. My expertise includes modern
-                        JavaScript frameworks, cloud infrastructure, and agile development practices.
+                        I am a passionate Computer Science undergraduate at the University of Kelaniya. My passion lies in web technologies, software development, and protecting digital assets. I strive to combine creativity with technical skills to solve complex challenges and contribute to a safer digital world. As an active member of university communities like FOSS and ISACA, I am dedicated to promoting open-source software and best practices in information security.
                     </p>
                 </div>
                 <div className="flex my-auto justify-center mx-auto">
